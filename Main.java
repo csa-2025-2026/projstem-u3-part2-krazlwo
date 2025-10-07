c723ee670670deba082e4e3b7c111a212f72df0f
krazlwo <12345esd.me@gmail.com>
2025-10-07 17:40:58 +0000

    oops

--- Main.java
+++ Main.java
@@ -6,7 +6,9 @@ public class Main
   {
     Scanner scan = new Scanner(System.in);  // input from the keyboard
     /* ============= BEGIN SOLUTION ================================ */
-
+  System.out.println("Problem 1")
+  Rectangle r1 = new Rectangle(3.2, 5.8);
+  
 
 
 
